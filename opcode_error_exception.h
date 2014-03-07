03a2669c9fc2aa6bb688ddb735326b391e890cd6
bryantto <bryanttto@gmail.com>
2014-03-07 15:57:10 -0800

    Create opcode_error_exception.h
    Error 404: Not enough bananas.

---
+++ opcode_error_exception.h
@@ -0,0 +1,35 @@
+/* opcode_error_exception.h
+  Team Texas - Jose Paterno, Jessica To, Quoc Lien, Bryant To
+  Masc xxxx
+	Exception class for opcodetab
+	CS530 Spring 2014
+	Alan Riggins
+*/
+
+#ifndef OPCODE_ERROR_EXCEPTION_H
+#define OPCODE_ERROR_EXCEPTION_H
+
+#include <string>
+
+using namespace std;
+
+class opcode_error_exception {
+	
+public:
+	opcode_error_exception(string message) {
+		this->message = message;
+	}
+
+	opcode_error_exception() {
+		message = "An error has occurred.";
+	}
+
+	string getMessage(){
+		return message;
+	}
+
+private:
+	string message;
+};
+
+#endif
